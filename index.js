ebd0cd8ca47b8f03796cc6e6bddc57e9e04ce23c
David Cho <davidhcho1095@gmail.com>
2022-04-29 14:30:47 -0400

    commit tree fdb4318207397f559853bb73bfbb814378d14082

---
+++ index.js
@@ -0,0 +1,8 @@
+//divide function
+const divide = (num1, num2) => num1 / num2
+let num1 = 2000
+let num2 = 100
+//Square arrow function
+const square = (num3, num4) => num3 * num3
+//Add arrow function
+const add = (num5, num6) => num5 + num6
